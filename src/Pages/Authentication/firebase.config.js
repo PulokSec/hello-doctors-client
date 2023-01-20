094054e84a3eddd379eba36ecb108ec05a7c0bf2
PulokSec <riopulok@gmail.com>
2023-01-20 17:49:36 +0600

    fix bugs

---
+++ src/Pages/Authentication/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDnJtcxyAw9G0CHQ1ebuJ2n7UxUwf3VP4o",
+  authDomain: "hello-doctors-528fc.firebaseapp.com",
+  projectId: "hello-doctors-528fc",
+  storageBucket: "hello-doctors-528fc.appspot.com",
+  messagingSenderId: "36497136487",
+  appId: "1:36497136487:web:376ad0398bfb14184c6dc2"
+};
+
+export default firebaseConfig;
